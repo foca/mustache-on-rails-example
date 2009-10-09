578b8851afb7503d080838abcd74f29e681ed4d2
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2009-10-09 14:58:03 -0200

    proxy methods to the AV instance
    not sure I like this, but it works and makes the code simpler. CON: you can't super into helpers, since they are not modules in the mustache - is that acceptable?

--- lib/mustache/rails.rb
+++ lib/mustache/rails.rb
@@ -3,35 +3,35 @@ require "mustache"
 class Mustache::Rails < Mustache
   attr_accessor :view
 
-  def partial(name)
-    name.strip!
-  end
-
-  # Delegate anything we don't understand back to the ActionView::Base instance
   def method_missing(method, *args, &block)
     view.send(method, *args, &block)
+  end
+
+  def respond_to?(method, include_private=false)
+    super(method, include_private) || view.respond_to?(method, include_private)
   end
 end
 
 class Mustache::Rails::TemplateHandler < ActionView::TemplateHandler
   def render(template, local_assigns, &block)
-    mustache_class_name = template.path.gsub(".html.mustache", "").classify
-    mustache_class = mustache_class_name.constantize
+    mustache = template.path.gsub(".html.mustache", "").classify.constantize
+    mustache.template_file = Rails.root.join("app", "templates", template.path)
 
-    result = mustache_class.new
-    copy_instance_variables_to(result)
-    result.template_file = Rails.root.join("app", "templates", template.path)
-    result.view          = @view
-    result[:yield]       = @view.instance_variable_get(:@content_for_layout)
-    result.to_html
+    returning mustache.new do |result|
+      copy_instance_variables_to(result)
+      result.view    = @view
+      result[:yield] = @view.instance_variable_get(:@content_for_layout)
+    end.to_html
   end
 
   def copy_instance_variables_to(mustache)
-    variables = @view.controller.instance_variable_names
+    variables  = @view.controller.instance_variable_names
     variables -= %w(@template)
+
     if @view.controller.respond_to?(:protected_instance_variables)
       variables -= @view.controller.protected_instance_variables
     end
+
     variables.each do |name|
       mustache.instance_variable_set(name, @view.controller.instance_variable_get(name))
     end
